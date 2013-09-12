b92433db72f086afc2314889034ee0de347cf7d1
Clayton Lengel-Zigich <clayton@claytonlz.com>
2013-09-11 17:05:51 -0700

    I don't even know what I just committed

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,9 +10,9 @@ set :deploy_to, "/home/deploy/apps/#{application}"
 set :user, "deploy"
 set :use_sudo, false
 
-role :web, "192.237.212.244"                          # Your HTTP server, Apache/etc
-role :app, "192.237.212.244"                          # This may be the same as your `Web` server
-role :db,  "192.237.212.244", :primary => true # This is where Rails migrations will run
+role :web, "waw.sprintreview.info"                          # Your HTTP server, Apache/etc
+role :app, "waw.sprintreview.info"                          # This may be the same as your `Web` server
+role :db,  "waw.sprintreview.info", :primary => true # This is where Rails migrations will run
 
 # if you want to clean up old releases on each deploy uncomment this:
 after "deploy:restart", "deploy:cleanup"
