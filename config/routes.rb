80457140a486107e7201c259d453572a89056268
Roy van de Water <dev+roy@integrumtech.com>
2013-09-11 19:50:05 -0700

    create navbar and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 WhereAgileWorks::Application.routes.draw do
+  get 'coach' => 'pages#coach'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -39,7 +40,7 @@ WhereAgileWorks::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
